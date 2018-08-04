6f701dc97e833dc906745ce7f5fbeb3069b4a83e
Liberta <antoine.libert@outlook.com>
2018-08-04 16:33:36 +0200

    Ajout du fichier index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Blog pour un écrivain</title>
+	<meta charset="utf-8">
+
+</head>
+<body>
+	<section>
+		<div>
+			
+		</div>
+	</section>
+</body>
+</html>
